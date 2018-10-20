7f5e92bf9854e90c86b2c95cf6be9c15220cae9d
Sean Cai-Mac <xcai95@gmail.com>
2018-10-20 17:57:00 -0400

    Changes

--- HW4/inv_pend.m
+++ HW4/inv_pend.m
@@ -1,10 +1,11 @@
-function xdot = inv_pend(t, x, w)
+function xdot = inv_pend(t, x, u)
 
 w_n = 1; 
 zeta = 0.1; 
+w = u;
 
 A = [0 1;...
-    -w_n.^2*(1+0.5*sin(w*t)) -2*zeta*w_n]; 
+    -w_n.^2*(1+0.5*sin(u*t)) -2*zeta*w_n]; 
 x = [x(1); x(2)];
 
 xdot = A*x;
