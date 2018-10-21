ec2839ab38ee06fe17c10dc89e0707496e286d13
Sean Cai-Windows <xcai95@gmail.com>
2018-10-20 21:52:31 -0400

    Changes

--- HW4/Problem_2.m
+++ HW4/Problem_2.m
@@ -8,23 +8,32 @@ clc
 
 %% Soluton
 tspan = [0:0.1:150]; 
-x0 = [1 0]; 
-%u = [0:0.1: 10]; % input
+x0 = [pi/2; 0]; 
+u = [0:0.1: 10]; % input
 options = odeset('RelTol', 1.0E-08, 'AbsTol', 1.0E-08);
-u = [0:0.1:10]; % input
 
 for i = 1:length(u)
     [tout, yout{i}] = ode45(@inv_pend, tspan, x0, options, u(i)); 
-    
+    RMS(i)=sqrt(sum(yout{i}(:, 1).^2)/length(u));
 end
 
-
 %% Plot the graph
-subplot(131)
-plot(tout, yout{18}); 
+subplot(311)
+plot(u, RMS); 
+xlabel('\omega')
+ylabel('RMS')
+title('RMS vs \omega')
 
-subplot(132)
+subplot(312)
 plot(tout, yout{19}); 
+ylabel('Amplitude')
+xlabel('Time (sec)')
+title('X(t) vs t when \omega = 1.9')
+legend('$ \theta $', '$\dot{ \theta }$', 'interpreter','latex')
 
-subplot(133)
+subplot(313)
 plot(tout, yout{20}); 
+ylabel('Amplitude')
+xlabel('Time (sec)')
+title('X(t) vs t when \omega = 2.0')
+legend('$ \theta $', '$\dot{ \theta }$', 'interpreter','latex')
